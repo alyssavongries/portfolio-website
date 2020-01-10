bc4512094bb38a705b570525d0f6377c255c53fa
av <40726835+alyssavongries@users.noreply.github.com>
2020-01-09 22:25:41 -0800

    Style and content update

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,33 @@
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += 'responsive'
   } else {
-    x.className = "navtoggle"
+    x.className = 'navtoggle'
   }
+}
+
+// modal
+var modal = document.getElementById('myModal')
+var images = document.getElementsByClassName('myImg')
+// the image in the modal img01 defined below
+var modalImg = document.getElementById('img01')
+// caption will be alt text
+var captionText = document.getElementById('caption')
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i]
+  // and attach our click listener for this image.
+  img.onclick = function (evt) {
+    modal.style.display = 'block'
+    modalImg.src = this.src
+    captionText.innerHTML = this.alt
+  }
+}
+
+var span = document.getElementsByClassName('close')[0]
+
+span.onclick = function () {
+  modal.style.display = 'none'
 }
