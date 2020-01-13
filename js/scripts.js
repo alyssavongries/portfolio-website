d79502ed4c89cd11988beba4a07648dd91a4bde4
av <40726835+alyssavongries@users.noreply.github.com>
2020-01-13 10:37:32 -0800

    JS corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,100 +1,77 @@
+// :::::::::::::::::::: mobile menuToggle
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += 'responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
 jQuery(document).ready(function() {
 
-function menuToggle () {
-  var x = document.getElementById('myNavtoggle')
-  if (x.className === 'navtoggle') {
-    x.className += 'responsive'
-  } else {
-    x.className = 'navtoggle'
+  // :::::::::::::::::::: modal
+  if ($('#myModal').length) {
+    var modal = document.getElementById('myModal');
+    var images = document.getElementsByClassName('myImg');
+    // the image in the modal img01 defined below
+    var modalImg = document.getElementById('img01');
+    // caption will be alt text
+    var captionText = document.getElementById('caption');
+
+    // Go through all of the images with our custom class
+    for (var i = 0; i < images.length; i++) {
+      var img = images[i];
+      // and attach our click listener for this image.
+      img.onclick = function (evt) {
+        modal.style.display = 'block';
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+      };
+    }
+
+    var span = document.getElementsByClassName('close')[0];
+
+    span.onclick = function () {
+      modal.style.display = 'none';
+    };
   }
-}
 
-// modal
-var modal = document.getElementById('myModal')
-var images = document.getElementsByClassName('myImg')
-// the image in the modal img01 defined below
-var modalImg = document.getElementById('img01')
-// caption will be alt text
-var captionText = document.getElementById('caption')
+  // :::::::::::::::::::: smooth scrolling
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+     &&
+     location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          });
+        }
+      }
+    });
 
-// Go through all of the images with our custom class
-for (var i = 0; i < images.length; i++) {
-  var img = images[i]
-  // and attach our click listener for this image.
-  img.onclick = function (evt) {
-    modal.style.display = 'block'
-    modalImg.src = this.src
-    captionText.innerHTML = this.alt
-  }
-}
-
-var span = document.getElementsByClassName('close')[0]
-
-span.onclick = function () {
-  modal.style.display = 'none'
-}
-
-// scrolling
-/*
-function scrollInk () {
-  var scrollLink = document.getElementsByClassName('scroll')
-  scrollLink.onclick = function (evt) {
-    $('html, body').stop().animate({
-      scrollTop: ((this).attr('href')).offset().top - 160
-    }, 400)
-    return false
-  } ('.scrollTop a').scrollTop()
-}
-scrollInk()
-
-$(document).ready(function(){
-  $('a[href^="#"]').click(function(e) {
-  e.preventDefault();
-
-  var target = this.hash,
-  $target = $(target);
-
-  $('html, body').stop().animate({
-      'scrollTop': $target.offset().top-100
-  }, 500, function () {
-      window.location.hash = target;
-  });
- });
 });
-
-/*
-function scrollInk() {
-  $('.scroll a').click(function(){
-$('html, body').stop().animate({
-      scrollTop: $( $(this).attr('href') ).offset().top - 160
-  }, 400);
-  return false;
-});
-$('.scrollTop a').scrollTop();
-}
-scrollInk();
-*/
-
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
