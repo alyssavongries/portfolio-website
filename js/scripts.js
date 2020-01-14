113bd90c5ece86aa78135a9115f06af98b5e7e14
av <40726835+alyssavongries@users.noreply.github.com>
2020-01-14 13:05:50 -0800

    User testing fixes

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 function menuToggle () {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
