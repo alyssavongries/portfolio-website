436083cb1801f43bf28f85cfcdf3e8741d4d56b7
av <40726835+alyssavongries@users.noreply.github.com>
2020-01-23 10:44:44 -0500

    Adds portfolio case study

--- js/scripts.js
+++ js/scripts.js
@@ -57,7 +57,7 @@ jQuery(document).ready(function() {
           // Only prevent default if animation is actually gonna happen
           event.preventDefault();
           $('html, body').animate({
-            scrollTop: target.offset().top
+            scrollTop: target.offset().top - 150
           }, 1000, function() {
             // Callback after animation
             // Must change focus!
